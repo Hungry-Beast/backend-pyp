0118848ee2d7422b913da397fa66935165153937
Hibjul Ahmed <hibjulahmed@gmail.com>
2022-03-02 17:14:22 +0530

    Routes added

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const subjectsRoute = require("./Routes/Subjects");
+const cors=require('cors')
 require("dotenv/config");
+
 const app = express();
 
-app.use(express.json());
-const PORT = process.env.PORT || 5001;
-console.log(process.env.DB_KEY);
-let DB_URL =
-    "mongodb+srv://developer:developer@cluster0.2logr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-let DB_URL2 =
-    "mongodb://developer:developer@cluster0-shard-00-00.2logr.mongodb.net:27017,cluster0-shard-00-01.2logr.mongodb.net:27017,cluster0-shard-00-02.2logr.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zgh7qu-shard-0&authSource=admin&retryWrites=true&w=majority";
-let DB_local = "mongodb://localhost:27017/test";
-// mongoose
-//     .connect(DB_local, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     .then(() => console.log("Db connected"));
-    try {
-        // Connect to the MongoDB cluster
-        mongoose.connect(
-          DB_URL,
-          { useNewUrlParser: true, useUnifiedTopology: true },
-          () => console.log(" Mongoose is connected")
-        );
-      } catch (e) {
-        console.log("could not connect");
-      }
-// .catch((error) => console.log(`${error} did not connect`));
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://developer:developer@cluster0.2logr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   console.log("Connected");
-//   // perform actions on the collection object
-//   client.close();
-// });
+// app.use(express.json());
+const PORT = process.env.PORT || 5000;
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
+app.use('/subjects',subjectsRoute)
+app.use("/", (req, res) => {
+    res.send("Hi chutiya");
+});
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+        process.env.DB_KEY,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose is connected")
+    );
+} catch (e) {
+    console.log("could not connect");
+}
+
 app.listen(PORT, () =>
     console.log(`Server Running on Port: http://localhost:${PORT}`)
 );
