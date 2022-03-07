51c1ec3e43f6853bcd66f8a62ce1c9fc74bc72f4
Subash Paul <subashpaul559@gmail.com>
2022-03-07 18:18:47 +0530

    Added /login API and middleware

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,23 +3,18 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
-const req = require('express/lib/request');
 const User = require('../Models/User');
-const JWT_SEC = 'backend-of-pyp';
+const fetchUser = require('../middleware/fetchUser')
+require("dotenv/config");
 
-//Creating a user in the DB
+const JWT_SEC = process.env.JWT_SEC;
+
+//Creating a route for aading an user to the DB
 router.post('/createuser', [
     body('name', 'Name must be atleast 3 chraracters').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters long').isLength({ min: 5 }),
-    // body('confPassword').custom((value, { req }) => {
-    //     if (value !== req.body.password) {
-    //         throw new Error('Password confirmation does not match password');
-    //     }
 
-    //     // Indicates the success of this synchronous custom validator
-    //     return true;
-    // }),
     body("regNumber", "Enter a valid Reg. No.").isLength({ max: 6 }),
 ], async (req, res) => {
     const errors = validationResult(req);
@@ -34,7 +29,6 @@ router.post('/createuser', [
         const reg = await User.findOne({
             regNumber: req.body.regNumber
         })
-        console.log(email, reg)
         if (email || reg) {
             return res.status(400).json("User already exists!")
         }
@@ -54,6 +48,38 @@ router.post('/createuser', [
         }
         const authToken = jwt.sign(data, JWT_SEC)
         res.status(200).json({ authToken })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Internal error occured")
+    }
+})
+// Route2: Creating a router for logging in.
+router.post('/login', [
+    body('name', 'Enter a valid name.').isLength({ min: 3 }),
+    body("password", "Enter a valid password").isLength({ min: 5 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, password } = req.body;
+    try {
+        let user = await User.findOne({ name })
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        const passCompare = await bcrypt.compare(password, user.password);
+        if (!passCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SEC)
+        res.status(200).json({ authToken })
+
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Internal error occured")
